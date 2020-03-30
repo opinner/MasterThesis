2efc961f3b1f378441300fa961356a384699b107
opinner <ole.pinner@yahoo.de>
2020-03-30 23:31:19 +0200

    finished animations (at least for now)

--- Analysis/ADCP/adcp_oxygen_animation.py
+++ Analysis/ADCP/adcp_oxygen_animation.py
@@ -1,5 +1,7 @@
 #---------------------------------------------------------------------------#
 #Creates a multidem#
+
+#TODO Change projection for scale display
 #---------------------------------------------------------------------------#
 
 import pathlib
@@ -14,10 +16,10 @@ from mpl_toolkits.basemap import Basemap
 from matplotlib import animation
 from matplotlib.gridspec import GridSpec
 
-
-offset = 3500 #4350
-max_frames = 4000
+offset = 3500 #4350#
+max_frames = 4000 #3000
 displayed_depth = 75
+
   
 ##########################################
 # Load ADCD data
@@ -50,7 +52,7 @@ all_west_east_flach = np.real(curr).T
 all_north_south_flach = np.imag(curr).T
 
 index_adcp_flach = np.argmin(np.abs(depth_flach-displayed_depth))
-print("index_adcp_flach",index_adcp_flach)
+print("index_adcp_flach",index_adcp_flach,depth_flach[index_adcp_flach])
 #interpolate to fill in missing data (possible due to a constant timestep)
 #index_adcp_flach = 29
 temp_x = all_west_east_flach[index_adcp_flach,:]
@@ -92,7 +94,7 @@ all_west_east_tief = all_west_east_tief[:,100:8165]
 
 
 index_adcp_tief = np.argmin(np.abs(depth_tief-displayed_depth))
-print("index_adcp_tief",index_adcp_tief)
+print("index_adcp_tief",index_adcp_tief, depth_tief[index_adcp_tief])
 
 #interpolate to fill in missing data (possible due to a constant timestep)
 #index_adcp_tief = 7
@@ -122,8 +124,8 @@ north_south_flach = north_south_flach[equal_time_index:]
 #print(np.shape(utc_flach),np.shape(utc_tief))
 
 if (utc_tief.size-utc_flach.size) > 0:
-    west_east_flach = np.pad(west_east_flach,(0,utc_tief.size - utc_flach.size),'constant', constant_values=0)
-    north_south_flach = np.pad(north_south_flach,(0,utc_tief.size - utc_flach.size),'constant', constant_values=0)
+    west_east_flach = np.pad(west_east_flach,(0,utc_tief.size - utc_flach.size),'constant', constant_values=np.nan)
+    north_south_flach = np.pad(north_south_flach,(0,utc_tief.size - utc_flach.size),'constant', constant_values=np.nan)
 
 
 
@@ -131,6 +133,38 @@ print(np.shape(west_east_flach),np.shape(west_east_tief))
 print(utc_flach[0],utc_flach[-1])
 print(utc_tief[0],utc_tief[-1])
 
+
+              
+#Load oxygen timeseries
+#------------------------- 
+data = np.load("/home/ole/windows/Preprocessing_TC_stations/PME/data/PME_emb217_TC_Flach_oxygen.npz",allow_pickle = True)
+#print(data.files)
+#concentration = data["concentration"]
+saturation_flach = data["saturation"]
+label_list_flach = data["label_list"]  #contains sensor ID and hight above the sea floor
+depth_at_TC_flach = 78
+PME_depth_flach = depth_at_TC_flach-label_list_flach[:,1].astype("float")
+print("PME_depth_flach")
+print(PME_depth_flach)
+
+PME_utc_flach = data["utc"]
+PME_index_flach = np.argmin(np.abs(PME_depth_flach-displayed_depth))
+print(PME_index_flach,PME_depth_flach[PME_index_flach])
+#print(np.shape(saturation_flach))
+    
+data = np.load("/home/ole/windows/Preprocessing_TC_stations/PME/data/PME_emb217_TC_Tief_oxygen.npz",allow_pickle=True)
+#print(data.files)
+#concentration = data["concentration"]
+saturation_tief = data["saturation"]
+label_list_tief = data["label_list"] #contains sensor ID and hight above the sea floor
+depth_at_TC_tief = 99
+PME_depth_tief = depth_at_TC_tief-label_list_tief[:,1].astype("float")
+print("PME_depth_tief")
+print(PME_depth_tief)
+
+PME_utc_tief = data["utc"]
+PME_index_tief = np.argmin(np.abs(PME_depth_tief-displayed_depth))
+print(PME_index_tief,PME_depth_tief[PME_index_tief])
 
 
 
@@ -165,16 +199,16 @@ emb217_flach = [57.3200,20.6150]
 emb217_tief = [57.3200,20.600]
 #print("emb217_tief",emb217_tief)
 
-
-output_picture = plt.figure()#constrained_layout=True)
-gs = GridSpec(2, 2, figure=output_picture, height_ratios = [4,1])
-curr_axis = output_picture.add_subplot(gs[0, :])
-tief_axis = output_picture.add_subplot(gs[1, 0])
-flach_axis = output_picture.add_subplot(gs[1, 1])
-
+##############################################################################################################
+##############################################################################################################
+##############################################################################################################
+#PLOTTING
+##############################################################################################################
+##############################################################################################################
+##############################################################################################################
 
 #Set up the picture:
-#output_picture, (curr_axis, flach_axis, tief_axis) = plt.subplots(nrows = 3, ncols = 1, gridspec_kw={'height_ratios': [3,1,1]})
+output_picture, (curr_axis, oxygen_axis) = plt.subplots(nrows = 2, ncols = 1, gridspec_kw={'height_ratios': [4, 1]})
 
 map_ax = Basemap(ax=curr_axis, llcrnrlon= XLLCORNER, llcrnrlat= YLLCORNER, urcrnrlon = XRCORNER, urcrnrlat = YRCORNER, suppress_ticks=False)
 #map_ax.drawcoastlines()
@@ -188,6 +222,7 @@ xx, yy = map_ax(lons,lats)
 
 #print(np.max(bathymetrie_array),np.min(bathymetrie_array))
 levels = np.arange(50,110,5)
+cmap_RdBu = plt.get_cmap('RdBu_r')
 cntrf = map_ax.contourf(xx, yy,bathymetrie_array, levels,cmap="Blues", latlon = True)
 cntr = map_ax.contour(xx, yy,bathymetrie_array, levels, colors ="black", latlon = True)
 
@@ -205,77 +240,56 @@ curr_axis.set_ylim(YLLCORNER,57.34)
 curr_axis.set_xlabel("longitude")
 curr_axis.set_ylabel("latitude")
 
-tief_axis.plot(utc_tief,west_east_tief[:],color = "tab:green", label = "WE")
-tief_axis.plot(utc_tief,north_south_tief[:],color = "tab:blue", label = "NS")
-flach_axis.plot(utc_tief,west_east_flach[:utc_flach.size],color = "tab:red", label = "WE")
-flach_axis.plot(utc_tief,north_south_flach[:utc_flach.size],color = "tab:orange", label = "NS")
 
-flach_axis.set_xlim(tief_axis.get_xlim())
 
-flach_axis.xaxis.set_major_locator(mdates.DayLocator())
-flach_axis.xaxis.set_minor_locator(mdates.HourLocator(byhour = [0,6,12,18],interval = 1))
+oxygen_axis.plot(PME_utc_flach,saturation_flach[PME_index_tief,:],color = "tab:red")
+oxygen_axis.plot(PME_utc_tief,saturation_tief[PME_index_tief,:],color = "tab:green")
+#oxygen_axis.plot(utc_tief,west_east_tief[:],color = "tab:red")
+#oxygen_axis.plot(utc_tief,north_south_tief[:],color = "tab:green")
+
+oxygen_axis.set_xlabel("2019")
+oxygen_axis.set_ylabel(r"O$_2$ saturation [%]")
+oxygen_axis.set_title("Oxygen measurements in "+str(displayed_depth)+" m depth", fontweight="bold")
+oxygen_axis.xaxis.set_major_locator(mdates.DayLocator())
+oxygen_axis.xaxis.set_minor_locator(mdates.HourLocator(byhour = [0,6,12,18],interval = 1))
 hfmt = mdates.DateFormatter('%d %b')
-flach_axis.xaxis.set_major_formatter(hfmt)
-flach_axis.set_title("shallow mooring", fontweight="bold")
-flach_axis.set_ylabel("velocity [m/s]")
-flach_axis.set_ylim(-0.2,+0.2)
-flach_axis.set_xlabel("2019")
+oxygen_axis.xaxis.set_major_formatter(hfmt)
+oxygen_axis.set_ylim(0,78)
+#oxygen_axis.set_ylim(-0.3,+0.3)
 
-tief_axis.set_xlabel("2019")
-tief_axis.set_title("deep mooring", fontweight="bold")
-tief_axis.set_ylabel("velocity [m/s]")
-
-tief_axis.xaxis.set_major_locator(mdates.DayLocator())
-tief_axis.xaxis.set_minor_locator(mdates.HourLocator(byhour = [0,6,12,18],interval = 1))
-hfmt = mdates.DateFormatter('%d %b')
-tief_axis.xaxis.set_major_formatter(hfmt)
-#oxygen_axis.set_ylim(0,78)
-tief_axis.set_ylim(-0.2,+0.2)
-
-
-
-leg_flach = flach_axis.legend(loc = "upper right")
-leg_tief = tief_axis.legend(loc = "upper left")
-
-for line_flach,line_tief in zip(leg_flach.get_lines(),leg_tief.get_lines()):
-    line_flach.set_linewidth(2.0)
-    line_tief.set_linewidth(2.0)
-    
-#output_picture.subplots_adjust(top=0.939,bottom=0.081,left=0.08,right=0.939,hspace=0.196,wspace=0.12)
-output_picture.subplots_adjust(top=0.944,bottom=0.066,left=0.06,right=0.979,hspace=0.166,wspace=0.12)
+output_picture.subplots_adjust(top=0.942,bottom=0.066,left=0.053,right=0.976,hspace=0.239,wspace=0.12)
 
 #axarr3[0].set_title(title_fig1,fontsize=16)
 #axarr3[1].set_title(title_fig2,fontsize=16) 
 
-output_picture.suptitle("ADCP measurements from emb217 in "+str(displayed_depth)+" m depth",fontsize=20,fontweight="bold")
+output_picture.suptitle("ADCP measurements from emb217 at "+str(displayed_depth)+" m depth",fontsize=20,fontweight="bold")
 
-output_picture.set_size_inches(18,10.5) #1.618*7.2,7.2)#
+output_picture.set_size_inches(16,10.5)
 
 
-scale = 0.9
+scale = 1
 
 quiver_flach = curr_axis.quiver([emb217_flach[1]],[emb217_flach[0]],[west_east_flach[0]],[north_south_flach[0]], color = "red", alpha = 1, scale = scale)
 quiver_tief = curr_axis.quiver([emb217_tief[1]],[emb217_tief[0]],[west_east_tief[0]],[north_south_tief[0]], color = "green", alpha = 1, scale = scale)
 
-current_time_tief, = flach_axis.plot([mdates.date2num(utc_tief[offset]),mdates.date2num(utc_tief[offset])],[-1,1],"k-",lw=2)    
-current_time_flach, = tief_axis.plot([mdates.date2num(utc_tief[offset]),mdates.date2num(utc_tief[offset])],[-1,1],"k-",lw=2)  
+current_time, = oxygen_axis.plot([mdates.date2num(utc_tief[offset]),mdates.date2num(utc_tief[offset])],[0,80],"k-",lw=2)
+
+plt.show()
 
 # initialization function: plot the background of each frame
 def init():
-    current_time_tief.set_data([],[])
-    current_time_flach.set_data([],[])
-    return tief_axis,flach_axis
+    current_time.set_data([],[])
+    return oxygen_axis,
 
-step = 1
 # animation function.  This is called sequentially
 def animate(i,quiver_flach,quiver_tief):
 
-    quiver_flach.set_UVC(west_east_flach[offset+i*step],north_south_flach[offset+i*step])
-    quiver_tief.set_UVC(west_east_tief[offset+i*step],north_south_tief[offset+i*step])
-    current_time_tief.set_data([mdates.date2num(utc_tief[offset+i*step]),mdates.date2num(utc_tief[offset+i*step])],[-1,1])
-    current_time_flach.set_data([mdates.date2num(utc_tief[offset+i*step]),mdates.date2num(utc_tief[offset+i*step])],[-1,1])
+    quiver_flach.set_UVC(west_east_flach[offset+i],north_south_flach[offset+i])
+    quiver_tief.set_UVC(west_east_tief[i],north_south_tief[offset+i])
+    current_time.set_data([mdates.date2num(utc_tief[offset+i]),mdates.date2num(utc_tief[offset+i])],[0,80])
+
     
-    return flach_axis,tief_axis,quiver_flach,quiver_tief,
+    return oxygen_axis,quiver_flach,quiver_tief,
 
 
 assert((max_frames+offset) < west_east_flach.size)
@@ -288,9 +302,7 @@ anim = animation.FuncAnimation(output_picture, animate, fargs = (quiver_flach,qu
 # the video can be embedded in html5.  You may need to adjust this for
 # your system: for more information, see
 # http://matplotlib.sourceforge.net/api/animation_api.html
-anim.save("currents_animation_"+str(displayed_depth)+"m.mp4", fps=45, extra_args=['-vcodec', 'libx264'])
-#anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+anim.save("oxygen_animation_"+str(displayed_depth)+"m.mp4", fps=30, extra_args=['-vcodec', 'libx264'])
 
 #plt.show()
-
 
