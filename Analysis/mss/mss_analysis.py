solved problem of wrong units (micromol per l instead of the previous micromol/kg)

Analysis/mss/mss_analysis.py
@@ -175,7 +175,7 @@ for FOLDERNAME in LIST_OF_MSS_FOLDERS:
             #print(lon)
             #print(np.all(np.diff(lon)>0))
             img4_0 = axarr4[0].plot(oxygen_sat_grid[profile_index,:],interp_pressure, label = "raw data")
-            img4_0a = axarr4[0].plot(bin_oxygen_sat_grid[profile_index,:],eps_pressure, label = "bined data")
+            img4_0a = axarr4[0].plot(bin_oxygen_sat_grid[profile_index,:],eps_pressure, label = "binned data")
             #img4_0b = axarr4[0].plot(0,BBL[profile_index],"Dr")
             #img4_0c = axarr4[0].plot(0,bathymetrie[profile_index],"Dg")
             #img4_0d = axarr4[0].plot(0,BBL_range[profile_index],"ok")
@@ -268,11 +268,11 @@ for FOLDERNAME in LIST_OF_MSS_FOLDERS:
             img3_1 = axarr3[1].pcolormesh(plotmesh_longitude,interp_pressure,oxygen_sat_grid.T,cmap = cmap_RdBu)
             img3_2 = axarr3[2].pcolormesh(plotmesh_longitude,eps_pressure,np.log10(eps_grid.T), vmax = -7, vmin = -9,cmap = cmap_hot)
             
-            BBL_img1 = BBL_ax[0].pcolormesh(plotmesh_longitude,interp_pressure,oxygen_sat_grid.T,cmap = cmap_RdBu)
-            BBL_img2 = BBL_ax[1].pcolormesh(plotmesh_longitude,eps_pressure,np.log10(eps_grid.T), vmax = -7, vmin = -9,cmap = cmap_hot)
+            BBL_img1 = BBL_ax[0].pcolormesh(plotmesh_longitude,bin_pressure,bin_oxygen_sat_grid.T,cmap = cmap_RdBu)
+            BBL_img2 = BBL_ax[1].pcolormesh(plotmesh_longitude,bin_pressure,np.log10(bin_eps_grid.T), vmax = -7, vmin = -9,cmap = cmap_hot)
                         
             
-            density_steps = np.arange(np.nanmin(pot_density_grid),np.nanmax(pot_density_grid),0.2)
+            density_steps = np.arange(np.nanmin(pot_density_grid),np.nanmax(pot_density_grid),0.3)
             #compute and plot isopycnals
             for density_step in density_steps:
                 isopycnal = np.zeros(number_of_profiles)
@@ -348,7 +348,7 @@ for FOLDERNAME in LIST_OF_MSS_FOLDERS:
             height = width / 1.618
             BBL_pic.set_size_inches(width,height)
             
-            colorbar(img1_0).set_label(r"Oxygen [$\mu mol/kg$]") 
+            colorbar(img1_0).set_label(r"Oxygen [$\mu mol/l$]") 
             colorbar(img1_1).set_label("salinity [SA]") 
             colorbar(img1_2).set_label("consv_temperature [C]")
             colorbar(img1_3).set_label(r"log10(dissipation) $[m^2 s^{-3}]$")

Analysis/mss/mss_bin_flux_transect.py
@@ -51,8 +51,8 @@ number_of_density_bins = density_bin_edges.size -1
 #density_bin_center = np.arange(1004,1010.5,0.2)
 
 
-f_flux,flux_axarr = plt.subplots(nrows = 2, ncols = 1, sharex = True, gridspec_kw={'height_ratios': [1.618, 1]}) 
-f_dissip,dissip_axarr = plt.subplots(nrows = 2, ncols = 1, sharex = True, gridspec_kw={'height_ratios': [1.618, 1]}) 
+f_flux,flux_axarr = plt.subplots(nrows = 2, ncols = 1, sharex = True, gridspec_kw={'height_ratios': [1.8, 1]}) 
+f_dissip,dissip_axarr = plt.subplots(nrows = 2, ncols = 1, sharex = True, gridspec_kw={'height_ratios': [1.8, 1]}) 
 f_interval,interval_axarr = plt.subplots(1) 
 #textstr = ""
 
@@ -151,7 +151,11 @@ for FOLDERNAME in LIST_OF_MSS_FOLDERS:
             eps_N_squared_grid = data["bin_N_squared_grid"]
             eps_density_grid = data["bin_density_grid"]
             eps_pot_density_grid = data["bin_pot_density_grid"]
-            #eps_pot_density_grid = np.sort(eps_pot_density_grid, axis = 0)
+
+            #sort density profiles
+            #eps_density_grid = thesis.sort_2D_array_with_nans(eps_density_grid)
+            #eps_pot_density_grid = thesis.sort_2D_array_with_nans(eps_pot_density_grid)
+
             #eps_viscosity_grid = data["eps_viscosity_grid"]
             eps_Reynolds_bouyancy_grid = data["bin_Reynolds_bouyancy_grid"]
             corrected_eps_Reynolds_bouyancy_grid = data["corrected_bin_Reynolds_bouyancy_grid"]
@@ -243,8 +247,8 @@ for FOLDERNAME in LIST_OF_MSS_FOLDERS:
             turbulent_diffusivity_Osborn_grid[profile,BBL_from:BBL_to] = law_of_the_wall_turbulent_diffusivity
             turbulent_diffusivity_Shih_grid[profile,BBL_from:BBL_to] = law_of_the_wall_turbulent_diffusivity
         
-        oxygen_gradient_grid = thesis.central_differences(eps_oxygen_grid)/thesis.central_differences(eps_depth)
-        unit_conversion_grid = 86400*(1000/eps_density_grid) #to convert from m*micromol/(kg*s) to mmol/(m^2*d)
+        oxygen_gradient_grid = thesis.central_differences(eps_oxygen_grid)/thesis.central_differences(eps_depth) #in units of micromol/(m*l)
+        unit_conversion_grid = 86400 #to convert from m*micromol/(l*s) to mmol/(m^2*d)  ; l to m^3 and micro to milli cancel each other out (factor of 1000)
     
         oxygen_flux_Osborn_grid = - turbulent_diffusivity_Osborn_grid * oxygen_gradient_grid * unit_conversion_grid
         oxygen_flux_Shih_grid = - turbulent_diffusivity_Shih_grid * oxygen_gradient_grid * unit_conversion_grid
@@ -259,6 +263,7 @@ for FOLDERNAME in LIST_OF_MSS_FOLDERS:
         
         
         for profile in range(number_of_profiles):
+        
         
             #skip profile is necessary
             if "_".join((cruise_name,transect_name,str(profile))) in list_of_bad_profiles:
@@ -676,7 +681,7 @@ for FOLDERNAME in LIST_OF_MSS_FOLDERS:
                 #try:
                 #print(start_interval_index,stop_interval_index,"\n")
                 assert start_interval_index < stop_interval_index
-                assert iso_mean_pressure[profile,start_interval_index] < iso_mean_pressure[profile,stop_interval_index]
+                #assert iso_mean_pressure[profile,start_interval_index] < iso_mean_pressure[profile,stop_interval_index]
                   
                      
                 #except AssertionError:
@@ -768,7 +773,8 @@ for FOLDERNAME in LIST_OF_MSS_FOLDERS:
     #np.savetxt("./data/"+cruise_name+'_iso_flux_results_woBBL.txt', np.transpose([longitude_list,distance_list,bathymetry_list,distance_lower_edge_iso_interval_sea_floor,mean_Osborn_flux,iso_vertical_mean_Osborn_flux,mean_Shih_flux,iso_vertical_mean_Shih_flux]), header = "longitude\tdistance\tdepth[dbar]\tdistance interval-ground\traw Osborn\trolling mean Osborn\traw Shih\trolling mean Shih", fmt = "%3.8f")
     
     np.savetxt("./data/"+cruise_name+'_iso_flux_results.txt', np.transpose([longitude_list,distance_list,bathymetry_list,distance_lower_edge_iso_interval_sea_floor,mean_Osborn_flux,iso_vertical_mean_Osborn_flux,mean_Shih_flux,iso_vertical_mean_Shih_flux]), header = "longitude\tdistance\tdepth[dbar]\tdistance interval-ground\traw Osborn\trolling mean Osborn\traw Shih\trolling mean Shih", fmt = "%3.8f")
-    
+
+    #np.savetxt("./data/"+cruise_name+'_iso_flux_results_sorted.txt', np.transpose([longitude_list,distance_list,bathymetry_list,distance_lower_edge_iso_interval_sea_floor,mean_Osborn_flux,iso_vertical_mean_Osborn_flux,mean_Shih_flux,iso_vertical_mean_Shih_flux]), header = "longitude\tdistance\tdepth[dbar]\tdistance interval-ground\traw Osborn\trolling mean Osborn\traw Shih\trolling mean Shih", fmt = "%3.8f")    
     ##################################################################################################################################
     ##################################################################################################################################
     ##################################################################################################################################
@@ -825,7 +831,7 @@ for FOLDERNAME in LIST_OF_MSS_FOLDERS:
 ###############################################################################################################
 ###############################################################################################################
 width = 6.2012
-height = width #* 4/3 #1.618
+height = width * 0.8 #* 4/3 #1.618
 
 #beamer figure sizes
 #width = 1.7*4.252 #6.2012
@@ -884,7 +890,7 @@ flux_axarr[0].set_ylabel(r"$\langle$OF$\rangle$ [mmol/(m$^2$d)]")
 #flux_axarr[0].set_ylabel(r"$\langle$Oxygen flux$\rangle$ [mmol/(m$^2$d)]")
 
 f_flux.set_size_inches(width,height) #set_size_inches(16,10.5)
-f_flux.subplots_adjust(top=0.95,bottom=0.09,left=0.12,right=0.975,hspace=0.058,wspace=0.185)
+f_flux.subplots_adjust(top=0.945,bottom=0.105,left=0.115,right=0.975,hspace=0.058,wspace=0.185) #top=0.95,bottom=0.09,left=0.12,right=0.975,hspace=0.058,wspace=0.185)
 #f_flux.subplots_adjust(top=0.98,bottom=0.09,left=0.12,right=0.975,hspace=0.058,wspace=0.185)
 
 props = dict(boxstyle='square', facecolor = "white")
@@ -918,7 +924,7 @@ dissip_axarr[1].yaxis.set_major_locator(mticker.MultipleLocator(pressure_tick_sp
 
 f_dissip.set_size_inches(width,height)
 #f_dissip.tight_layout() 
-f_dissip.subplots_adjust(top=0.95,bottom=0.09,left=0.125,right=0.975,hspace=0.058,wspace=0.185) #top=0.925,bottom=0.1,left=0.137,right=0.965,hspace=0.153,wspace=0.2)
+f_dissip.subplots_adjust(top=0.945,bottom=0.105, left = 0.13,right=0.975,hspace=0.058,wspace=0.185) #top=0.95,bottom=0.09,left=0.125,right=0.975,hspace=0.058,wspace=0.185)
 #f_dissip.subplots_adjust(top=0.98,bottom=0.09,left=0.125,right=0.975,hspace=0.058,wspace=0.185)
     
 #props = dict(boxstyle='square', facecolor = "white")